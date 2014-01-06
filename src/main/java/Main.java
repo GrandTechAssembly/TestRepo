d93761dad6f40fd3828e58f071a864c6a2ddeeed
Grand Tech <grandtechassembly@gmail.com>
2014-01-06 16:23:03 +0100

    bla

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+package main.java;
+
+class HelloWorldApp {
+    public static void main(String[] args) {
+    	String hello = "Hello World";
+        System.out.println(hello); // Display the string.
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+
+	    String str1 = "Hello World!";
+	    String str2 = "Hello World!";
+	    assertTrue(str1.equals(str2));
+	  }
+	
+}
